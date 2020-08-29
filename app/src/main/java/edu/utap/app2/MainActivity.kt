c76385c2585fd71c283dd341fe42f99fc18bff93
Alex Martin <arm2216@columbia.edu>
2020-08-29 18:40:19 -0500

    Initial Commit

---
+++ app/src/main/java/edu/utap/app2/MainActivity.kt
@@ -0,0 +1,19 @@
+package edu.utap.app2
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+    }
+
+}
